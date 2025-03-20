3fbe36f869c7a3b4bfce3661a417b9546643e049
David Wei Dai <david.dai.personal@gmail.com>
2025-03-20 16:49:59 +1100

    Create RemoveDupFromSortedArray.kt

---
+++ RemoveDupFromSortedArray.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun removeDuplicates(nums: IntArray): Int {
+        if (nums.isEmpty()) {
+            return 0
+        }
+
+        var i = 0
+        for (j in 1 until nums.size) {
+            if (nums[i] != nums[j]) {
+                nums[++i] = nums[j]
+            }
+        }
+
+        return i + 1
+    }
+}
