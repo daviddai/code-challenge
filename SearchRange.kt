6628fb0f225ba6cec8e8696dc59e6a1dfa19bed9
David Wei Dai <david.dai.personal@gmail.com>
2025-03-24 08:56:41 +1100

    Update SearchRange.kt

--- SearchRange.kt
+++ SearchRange.kt
@@ -17,3 +17,43 @@ class Solution {
         return intArrayOf(start, end)
     }
 }
+
+
+class Solution {
+    fun searchRange(nums: IntArray, target: Int): IntArray {
+        if (nums.size == 0) {
+            return intArrayOf(-1, -1)
+        }
+
+        var left = 0
+        var right = nums.size - 1
+        var pt = -1
+
+        while (left <= right) {
+            pt = (left + right) / 2
+            if (nums[pt] == target) {
+                break
+            } else if (nums[pt] < target) {
+                left = pt + 1
+            } else {
+                right = pt - 1
+            }
+        }
+
+        if (nums[pt] != target) {
+            return intArrayOf(-1, -1)
+        } else {
+            left = pt
+            while (left > 0 && nums[left - 1] == target) {
+                --left
+            }
+
+            right = pt
+            while (right < nums.size - 1 && nums[right + 1] == target) {
+                ++right
+            }
+
+            return intArrayOf(left, right)
+        }
+    }
+}
